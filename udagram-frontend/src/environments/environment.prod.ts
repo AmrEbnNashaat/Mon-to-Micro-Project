1a0e0f552e31ce5f54ef4e453f6730815a5a8290
AmrEbnNashaat <slayerxd@live.com>
2023-02-18 04:07:18 +0200

    Project Submision

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a084cbd823c9a4616badaae28a166c6d-2099627846.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
